1db6e8007213d2aacefae56e1ea957064a5e8e10
nfarhatclarience <137424109+nfarhatclarience@users.noreply.github.com>
2023-07-12 08:25:35 -0400

    Added Karatsuba for arbitrary numbers

--- Scientific/RecursiveIntegerMultiplication/RecursiveIntegerMultiplication.py
+++ Scientific/RecursiveIntegerMultiplication/RecursiveIntegerMultiplication.py
@@ -1,48 +1,47 @@
 ''' this program implements the pseudocode for recursive integer multiplication @page 9 of the book Algorithm Illuminated Part 1: The Basics'''
-
-''' Pseudocode 
-recursiveIntegerMultiplication
-Input: Two n-digit positive integers number1 and number2
-Output: The product number1*number2
-Assumption : n is a power of 2, and x and y have the same number of digits, n
-# Base case
-if n = 1 then
-    return number1*number2
-# Recursive case
-else
-    a,b = first and second halves of number1, where x = 10**(n/2)*a + b
-    c,d = first and second halves of number2, where y = 10**(n/2)*c + d
-    compute ac = a*c recursively
-    compute bd = b*d recursively
-    conpute ad = a*d recursively
-    compute bc = b*c recursively
-    return 10**n*ac + 10**(n/2)*(ad + bc) + bd
-
-karatsubaMultiplication very similar to above but with 3 recursive calls instead of 4
-Input: Two n-digit positive integers number1 and number2
-Output: The product number1*number2
-Assumption : n is a power of 2, and x and y have the same number of digits, n
-# Base case
-if n = 1 then
-    return number1*number2
-# Recursive case
-else
-    a,b = first and second halves of number1, where x = 10**(n/2)*a + b, then a = x//10**(n/2), b = x % 10**(n/2)
-    c,d = first and second halves of number2, where y = 10**(n/2)*c + d
-    p = a+b
-    q = c+d
-    
-    compute ac = a*c recursively
-    compute bd = b*d recursively
-    compute pq = p*q recursively
-    compute adbc = pq - ac - bd
-    return 10**n*ac + 10**(n/2)*(adbc) + bd
-'''
+''' Check Karatsuba.pdf for the discussion of the Algorithm''' 
+''' it works for numbers of the same size and the size can be arbitrary'''
+''' itoverflows for numbers of size 10**9 and above'''
 
 import numpy as np
 import unittest
 import time 
-        
+import sys
+
+
+
+# get number of digits in a number using log10
+def getNumDigits(number):
+    number = np.double(number)
+    if number == 0:
+        return 1
+    if np.floor(np.log10(number)) == 0:
+        return 1 
+    if number < 0:
+        number = -number
+    return np.floor(np.log10(number)) + 1
+# get number of 2 multiples in a number using log2
+def getNum2Multiples(number):
+
+    return np.floor(np.log2(number)) + 1
+
+def getPaddingsize(number):
+    '''Pad a number with zeros to make it a power of 2'''
+    num_digits = getNumDigits(number)
+    if num_digits == 1:
+        return 0
+    num_zeros = 2**getNum2Multiples(num_digits) - num_digits
+    return num_zeros
+
+def padNumber(number,pading_size):
+    '''Pad a number with zeros to make it a power of 2'''
+    padding = number * 10**int( pading_size)
+    return padding
+def unpadNumber(number,pading_size):
+    '''removed padded zeros from a number'''
+    return number // 10**int( pading_size)
+
+# write a unit test to check if the padding and unpadding works
 def recursiveIntegerMultiplication(number1, number2,n1,n2):
     '''Assumption: number1 and number2 are of the same size and there size is a power of 2'''
     # Base case
@@ -65,38 +64,48 @@ def recursiveIntegerMultiplication(number1, number2,n1,n2):
 
 
 
-
-
-
-class TestRecursiveIntegerMultiplication(unittest.TestCase):
-
-    def test_recursive_multiplication_random(self):
-        number_of_tests = 3000
-        print(f"Generating {number_of_tests} tests \n")
-        start = time.time()
-        for i in range(number_of_tests):
-                n = 8
-                # Generate two random numbers of size n**2
-                num1 = np.random.randint(10**(n-1), 10**n -1)
-                num2 = np.random.randint(10**(n-1), 10**(n) -1)
-                n1 = n2 = 8
-                # Check if the product computed by the recursive algorithm is the same as the built-in multiplication result
-        
-                assert np.abs(recursiveIntegerMultiplication(num1, num2, n1, n2) - (num1 * num2)) == 0, f"Test failed for num1={num1}, num2={num2}, n={n}"
-        end = time.time()
-        print(f"Time taken for {number_of_tests} tests is {end-start} seconds")
-
-    def recursive_multiplication_chap1_ex_1_8(self):
-        n1 = 3141592653589793238462643383279502884197169399375105820974944512    
-        n2 = 2718281828459045235360287471352662497757247093699959574966967612
-        n = len(str(n1))
-        product_recursive = recursiveIntegerMultiplication(n1, n2, n, n)
-        product_builtin = n1*n2
-        #error in the recursive algorithm
-        assert  np.abs(product_recursive - product_builtin) == 0, f"Test failed for num1={n1}, num2={n2}, n={n}"
-
-
-        
-if __name__ == '__main__':
-    unittest.main()
+def karatsubaMultiplication(number1, number2,n1,n2):
     
+    '''Assumption: number1 and number2 are of the same size and there size is a power of 2'''
+    # Base case
+    if n1== 1:
+        return number1 * number2
+    # Recursive case
+    else:
+        a = number1 // 10**(n1//2)
+        b = number1 % 10**(n1//2) 
+        c = number2 // 10**(n2//2) 
+        d = number2 % 10**(n2//2) 
+        p = a+b
+        q = c+d
+        n3 = n1//2
+        n4 = n2//2  
+        ac = karatsubaMultiplication(a,c,n3,n4)
+        bd = karatsubaMultiplication(b,d,n3,n4)
+        pq = karatsubaMultiplication(p,q,n3,n4)
+        adbc = pq - ac - bd
+        return 10**(n1)*ac + 10**(n3)*(adbc) + bd
+
+# main function
+def main():
+    n1 = 31415921
+    n2 = 27182811
+    print(sys.getsizeof(int(n1)))
+    #n1 = 123411
+    #n2 = 567811
+    number_of_digits1 = getNumDigits(n1)
+    number_of_digits2 = getNumDigits(n2)
+    print(f"number_of_digits1 = {number_of_digits1}, number_of_digits2 = {number_of_digits2}")
+    padding_1 = getPaddingsize(n1)
+    padding_2 = getPaddingsize(n2)
+    print(f"padding_1 = {padding_1}, padding_2 = {padding_2}")
+    padded_n1 = padNumber(n1,padding_1)
+    padded_n2 = padNumber(n2,padding_2)
+    print(f"padded_n1 = {padded_n1}, padded_n2 = {padded_n2}")
+    result = karatsubaMultiplication(padded_n1,padded_n2,number_of_digits1+padding_1,number_of_digits2+padding_2)
+    result = unpadNumber(result,padding_1+padding_2)
+    print(f"result = {result}")
+    #error in result 
+    print(f"error = {result - n1*n2}")
+if __name__ == '__main__':
+    main()
