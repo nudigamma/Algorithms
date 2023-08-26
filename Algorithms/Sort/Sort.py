9c1d4e6344810444d6537d28f4cf93ee82c9a0aa
Nadim Farhat <nadim.farhat@gmail.com>
2023-08-26 15:26:41 -0400

    Refactored MergeSort to call Merge as seperate funciton

--- Algorithms/Sort/Sort.py
+++ Algorithms/Sort/Sort.py
@@ -17,7 +17,6 @@ def InsertionSort(array):
     return array
 
 
-
 def swapelement(i,j):
     '''swaps the ith and jth element of the array'''
     '''Helper function for bubble sort below'''
@@ -57,44 +56,50 @@ def BubbleSort(array,verbose=False):
 
     return array,count
 
+# Merge 
+def Merge(right,left,merged):
+    i = 0
+    j = 0
+    k = 0
+    # while we are within the limits of the arrays
+    # we merge the arrays
+    while (i < right.shape[0]) and (j < left.shape[0]):
+        if right[i] < left[j]:
+            merged[k] = right[i]
+            i += 1
+        else: 
+            merged[k] = left[j] 
+            j += 1
+            k += 1
+    # we exited the above loop due to one of the arrays being exhausted
+    # we now copy the remaining elements from the other array
+    # if we are still within the limits of the right array we copy the remaining elements
+    while i < right.shape[0]:
+        merged[k] = right[i]
+        i += 1
+        k += 1
+    # if we are still within the limits of the left array we copy the remaining elements
+    while j < left.shape[0]:
+        merged[k] = left[j]
+        j += 1
+        k += 1
+        # we return the merged array
+    return merged 
+
 def MergeSort(array):
     '''This function implements the merge sort algorithm
     Input: numpy array of numbers
     Output: sorted numpy array of numbers
     as presented in the book, Algorithms Illuminated by Tim Roughgarden'''
-    c = np.zeros(array.shape[0])
+    merged = np.zeros(array.shape[0])
     # base case
     if array.shape[0] == 1:
         return array
     else:
         # general case
         # we split the array in two and recursively call mergeSort
+        left =  MergeSort(array[array.shape[0]//2:])
         right = MergeSort(array[0:array.shape[0]//2])
-        left = MergeSort(array[array.shape[0]//2:])
-        i = 0
-        j = 0
-        k = 0
-        # while we are within the limits of the arrays
-        # we merge the arrays
-        while (i < right.shape[0]) and (j < left.shape[0]):
-                if right[i] < left[j]:
-                    c[k] = right[i]
-                    i += 1
-                else: 
-                    c[k] = left[j] 
-                    j += 1
-                k += 1
-        # we exited the above loop due to one of the arrays being exhausted
-        # we now copy the remaining elements from the other array
-        # if we are still within the limits of the right array we copy the remaining elements
-        while i < right.shape[0]:
-                c[k] = right[i]
-                i += 1
-                k += 1
-        # if we are still within the limits of the left array we copy the remaining elements
-        while j < left.shape[0]:
-                c[k] = left[j]
-                j += 1
-                k += 1
-        # we return the merged array
-        return c
+        merged = Merge(right,left,merged)
+        return merged
+    
---
+++ TestsAndValidations/test_merge_sort.py
@@ -0,0 +1,31 @@
+'''This file implements the merge sort algorithm.'''
+
+import numpy as np
+import unittest
+import sys
+import socket 
+import os
+# get os type
+import os 
+import sys 
+import platform
+
+    
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+print(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import Utils.RandomDataGenerator as rnd
+from Algorithms.Sort.Sort import MergeSort
+
+class TestMergeSort(unittest.TestCase):
+    def test_merge_sort(self):
+        number_of_tests = 3000
+        print(f"Generating {number_of_tests} tests \n")
+        for i in range(number_of_tests):
+            array = rnd.generateRandomArray(1000,2000,0,10)
+            error = np.sort(array)-MergeSort(array)
+        assert error.all() == 0
+        print(f"Completed {number_of_tests} tests \n")  
+
+if __name__ == '__main__':
+    unittest.main()
