3f8abf2226160c767e11bf78132ea2c3bd2c3b7e
nudigamma <nadim.farhat@gmail.com>
2023-09-17 11:50:12 -0400

    Working Closest Pair Code

--- Algorithms/Search/Search.py
+++ Algorithms/Search/Search.py
@@ -21,7 +21,7 @@ def closestPairBruteForce1D(array):
     for x in range(len(array)):
         # we find the squared distance be
         for next_element in range(x,len(array)):
-                d_2= (array[next_element] - array[x])**2 
+                d_2= distance_squared(array[x],array[next_element])
                 if  d_2 < minDistance:
                     minDistance = d_2
                     min_p1, min_p2 =  array[x], array[next_element]
@@ -65,8 +65,7 @@ def closestDistance2DBruteForce(array):
     for element in range(len(array)):
         # we find the squared distance be
         for next_element in range(element,len(array)):
-                d_2= (array[element][0] - array[next_element][0])**2 + (array[element][1] - array[next_element][1])**2
-                print(d_2)
+                d_2= distance_squared(array[element],array[next_element])
                 if  d_2 < minDistance and d_2 != 0:
                     minDistance = d_2
                     min_p1, min_p2 =  array[element], array[next_element]
@@ -79,35 +78,80 @@ def distance_squared(p1,p2):
     return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
 
 
-def FindclosestPair(ArrayOfPoints):
-    '''This function finds the closest pair using the divide and conquer algorithm'''
-    if len(ArrayOfPoints) == 0 or len(ArrayOfPoints) == 1:
-        raise ValueError("input array needs to have more than one element")
-    if len(ArrayOfPoints) == 2:
-        return ArrayOfPoints[0],ArrayOfPoints[1]
+import numpy as np
+import platform
+import sys
+import os 
+
+from Algorithms.Sort.Sort import MergeSort
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+def closestPairBruteForce1D(array):
+    pass
+
+def closestPoint1D(array):
+    ''' docstring for closestPoint1D'''
     
-    #sort the array according to x coordinate
-    ArrayOfPointsX = sorted(ArrayOfPoints, key=lambda x: x[0]) # assuming using mergesort   
-    ArrayOfPointsY = sorted(ArrayOfPoints, key=lambda x: x[1]) # assuming using mergesort
-    #divide the array into two halves
-        #find the closest pair in the left half
-    l1, l2 = closestPair(Lx,Ly)
+    '''Find the closess Pair of points in a 1D array
+    Args:
+        array: a numpy array of integers
+    Returns:
+        a tuple of two integers
+    Raises:
+        ValueError: if the input array has less than two elements
+    '''
+    
+    if len(array) == 0 or len(array) == 1:
+        raise ValueError("input array needs to have more than one element")
+    #sort the array
+    array = MergeSort(array)
+    minDistance = np.inf
+    min_p1, min_p2 = None, None
+    for x in range(len(array)-1):
+        d_2= (array[x+1] - array[x])**2 
+        if  d_2 < minDistance:
+            minDistance = d_2
+            min_p1, min_p2 =  array[x], array[x+1]
+    return min_p1,min_p2
+
+
+def closestDistance2DBruteForce(array):
+    minDistance = np.inf
+    if len(array) == 0 or len(array) == 1:
+        raise ValueError("input array needs to have more than one element")
+
+    min_p1, min_p2 = None, None
+    minDistance = np.inf
+    # 2D array meaning it doesn't have y coordinate. 
+    for element in range(len(array)):
+        # we find the squared distance be
+        for next_element in range(element+1,len(array)):
+                d_2 = distance_squared(array[element],array[next_element])
+                if  d_2 < minDistance :
+                    minDistance = d_2
+                    min_p1, min_p2 =  array[element], array[next_element]
+
+    return min_p1,min_p2
+
+
+def distance_squared(p1,p2):
+    #find the 2D distance between two points
+    return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
 
 
 def closestPair(SortedInX,SortedInY):
     '''Find the closess Pair of points in a 2D array '''
     # SortedInX will have at least three elements because of the findClosestPair condition on input 
-    if len(SortedInX) <= 3: # find the smallest distance between the three points
-        minDistance = np.inf
-        min_p1, min_p2 = None, None
-        for x in range(len(SortedInX)-1):
-            for next_element in range(x,len(SortedInX)):
-                #find the squared distance between two points
-                d_2= distance_squared(SortedInX[x],SortedInX[next_element])
-                if  d_2 < minDistance:
-                    minDistance = d_2
-                    min_p1, min_p2 =  SortedInX[x], SortedInX[next_element]
-        return min_p1,min_p2
+    if len(SortedInX) == 0 :
+        return (np.inf,np.inf),(np.inf,np.inf)
+    if len(SortedInX) == 1:
+        return SortedInX[0],(np.inf,np.inf)
+    if len(SortedInX) == 2:
+        return SortedInX[0],SortedInX[1]
+    
+    if len(SortedInX) == 3: # find the smallest distance between the three points
+        return closestDistance2DBruteForce(SortedInX)
+   
     #no need for else condition because if will return
     #divide the SortedInX into two halves
     Lx = SortedInX[0:len(SortedInX)//2]
@@ -123,17 +167,104 @@ def closestPair(SortedInX,SortedInY):
     d2 = distance_squared(r1,r2)
     delta = min(d1,d2)
     #find the closest pair that has one point in the left half and one point in the right half
+    #s1,s2 = closestSplitPair(SortedInX,SortedInY,delta)
+    #return the closest pair
+    #d3 = distance_squared(s1,s2)
+    return l1, l2, r1, r2, min(d1,d2)
+
+
+def FindclosestPair(ArrayOfPoints):
+    '''This function finds the closest pair using the divide and conquer algorithm'''
+    if len(ArrayOfPoints) == 0 or len(ArrayOfPoints) == 1:
+        raise ValueError("input array needs to have more than one element")
+    if len(ArrayOfPoints) == 2:
+        print(f"ArrayOfPoints = {ArrayOfPoints}")
+        return ArrayOfPoints[0],ArrayOfPoints[1]
+    
+    #sort the array according to x coordinate
+    
+    PointsX = sorted(ArrayOfPoints, key=lambda x: x[0]) 
+    PointsY = sorted(ArrayOfPoints, key=lambda x :x[1])
+    
+    #divide the array into two halves
+    #find the closest pair in the left half
+    
+    return  (closestPair(PointsX,PointsY))
+
+def closestPair(SortedInX,SortedInY):
+    '''Find the closess Pair of points in a 2D array '''
+    # SortedInX will have at least three elements because of the findClosestPair condition on input 
+    
+    
+    
+    if len(SortedInX) <= 3: # find the smallest distance between the three points
+        return closestDistance2DBruteForce(SortedInX)
+    
+   
+    #no need for else condition because if will return
+    #divide the SortedInX into two halves
+    Lx = SortedInX[0:len(SortedInX)//2]
+    Rx = SortedInX[len(SortedInX)//2:]
+    #Ly is lx sorted according to y coordinate
+    Ly = sorted(Lx,key=lambda x:x[1])
+    #Ry is rx sorted according to y coordinate
+    Ry = sorted(Rx,key=lambda x:x[1])
+
+    #find the closest pair in the left half
+    l1, l2 = closestPair(Lx,Ly)
+    #find the closest pair in the right half
+    r1, r2 = closestPair(Rx,Ry)
+    d1 = distance_squared(l1,l2)
+    d2 = distance_squared(r1,r2)
+    delta = min(d1,d2)
+    print(f"delta = {delta}")
+    #find the closest pair that has one point in the left half and one point in the right half
     s1,s2 = closestSplitPair(SortedInX,SortedInY,delta)
     #return the closest pair
     d3 = distance_squared(s1,s2)
-    return min(min(d1,d2),d3)
+    if d1 < d2:
+        if d1 < d3:
+            return l1, l2
+        else:
+            return s1,s2
+    else:
+        if d2 < d3:
+            return r1, r2
+        else:
+            return s1,s2
 
 
 
-def closesSplitPair(SortedInX,SortedInY,delta):
-    pass
+def closestSplitPair(SortedInX, SortedInY, delta):
+    # Find the middle point, it should be the last point in the left half
+    middlePoint = SortedInX[len(SortedInX) // 2][0]
+    print(f"middlePoint = {middlePoint}")
+    
+    # Find the points that belong to Sy which are between middlePoint - delta and middlePoint + delta
+    Sy = [point for point in SortedInY if middlePoint - delta <= point[0] <= middlePoint + delta]
+    print(f"Sy = {Sy}")
+    
+    # Check if Sy has less than two points
+    if len(Sy) < 2:
+        return (-np.inf, -np.inf), (np.inf, np.inf)  # Return a default pair; this will be ignored due to the larger distance.
+    
+    # Initialize the ClosestPair to the first two points of Sy
+    ClosestPair = Sy[0], Sy[1]
+    ClosestDistance = delta #distance_squared(Sy[0], Sy[1])
+    
+    for i in range(len(Sy) - 1):
+        for j in range(i + 1, min(i + 8, len(Sy))):
+            d = distance_squared(Sy[i], Sy[j])
             
-
+            # Ensure we're not comparing the same point against itself
+            if d == 0:
+                continue
+                
+            if d < ClosestDistance:
+                ClosestDistance = d
+                ClosestPair = Sy[i], Sy[j]
+    
+    return ClosestPair[0], ClosestPair[1]
 
 def SecondLargest(array):
     '''This function finds the second largest element in an array'''
