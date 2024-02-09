710f73805673fa48826c6490d4b515314e301ff5
nudigamma <nadim.farhat@gmail.com>
2024-02-09 15:18:53 -0500

    Restructured the directories again to be more flat and  less depedencies

--- Python/Utils/RandomDataGenerator.py
+++
@@ -1,9 +0,0 @@
-'''This utility class is used to generate random data for testing purposes.'''
-import numpy as np
-
-
-'''This function generats a random array of integers of random size between user specified min and max size.'''
-
-def generateRandomArray(minSize, maxSize, minValue, maxValue):
-    size = np.random.randint(minSize, maxSize)
-    return np.random.randint(minValue, maxValue, size)
