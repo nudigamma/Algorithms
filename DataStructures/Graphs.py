7d94b760f1ba71b7ef217a217b6879372a01b025
nfarhatclarience <nadim.farhat@gmail.com>
2024-03-23 08:03:12 -0400

    Rewrote the Graphs.py file to use a dictionary to store the graph.

--- DataStructures/Graphs.py
+++ DataStructures/Graphs.py
@@ -1,34 +1,21 @@
+'''this class implements a graph data structure using an adjacency list representation'''
+'''the graph is represented as a dictionary 
+where the keys are the vertices and the values are lists of
+the vertices that are adjacent to the key vertex, no parallel edges are allowed'''
+
+
+
 class Graph:
     def __init__(self):
-        self.graph = dict()
+        self.graph = {}
+        
+    def add_vertex(self, vertex : int ) -> None:
+        if self.graph.get((str(vertex))) == None :
+            self.graph[str(vertex)] = []
+    def connect(self, vertex1 :int, vertex2:int ) -> None:
+        if self.graph.get(str(vertex1)) != None and self.graph.get(str(vertex2)) != None: 
+            if vertex2 not in self.graph[str(vertex1)]:
+                self.graph[str(vertex1)].append(vertex2)
+            if vertex1 not in self.graph[str(vertex2)]:
+                self.graph[str(vertex2)].append(vertex1)
 
-    def add_vertex(self, vertex):
-        if vertex not in self.graph:
-            self.graph[vertex] = []
-
-    def add_edge(self, vertex1, vertex2, bidirectional=False):
-        # Ensure both vertices exist
-        self.add_vertex(vertex1)
-        self.add_vertex(vertex2)
-
-        # Add edge from vertex1 to vertex2
-        if vertex2 not in self.graph[vertex1]:
-            self.graph[vertex1].append(vertex2)
-
-        # For undirected graphs, add edge from vertex2 to vertex1 as well
-        if bidirectional and vertex1 not in self.graph[vertex2]:
-            self.graph[vertex2].append(vertex1)
-
-    def add_list(self, vertex, lst):
-        # Ensure the vertex exists
-        self.add_vertex(vertex)
-
-        # Optionally: Validate vertices in lst exist or add them
-        for v in lst:
-            self.add_vertex(v)
-
-        self.graph[vertex] = lst
-
-    def print_graph(self):
-        for vertex in sorted(self.graph.keys()):  # Sorting vertices for a more organized output
-            print(f"{vertex} -> {self.graph[vertex]}")
